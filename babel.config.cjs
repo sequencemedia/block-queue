ef4dde45abb33fa8347dab69b354cfa0c427d0a7
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-11-27 22:49:23 +0000

    Babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,30 +1,10 @@
-const debug = require('debug')
-
-const {
-  env: {
-    DEBUG = '@sequencemedia/block-queue'
-  } = {}
-} = process
-
-debug.enable(DEBUG)
-
-const log = debug('@sequencemedia/block-queue')
-
 const {
   env: {
     NODE_ENV = 'development'
   }
 } = process
 
-log('`block-queue` is awake')
-
-function env () {
-  log({ NODE_ENV })
-
-  return (
-    NODE_ENV === 'production'
-  )
-}
+const env = () => NODE_ENV === 'production'
 
 const presets = [
   [
@@ -38,15 +18,10 @@ const presets = [
   ]
 ]
 
-const plugins = []
-
 module.exports = (api) => {
   if (api) api.cache.using(env)
 
   return {
-    compact: true,
-    comments: false,
-    presets,
-    plugins
+    presets
   }
 }
